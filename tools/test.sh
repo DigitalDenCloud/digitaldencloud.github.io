2250c748eb7cd7abfbb8f19495141bf2e90ed0d7
Deniz Yilmaz <denizyilmaz@hotmail.co.uk>
2025-12-05 18:50:05 +0000

    fix(ci): simplify htmlproofer command to use committed config

--- tools/test.sh
+++ tools/test.sh
@@ -61,9 +61,9 @@ main() {
     -d "$SITE_DIR$_baseurl" -c "$_config"
 
   # test
-  bundle exec htmlproofer "$SITE_DIR" \
-    --disable-external \
-    --ignore-urls "/^http:\/\/127.0.0.1/,/^http:\/\/0.0.0.0/,/^http:\/\/localhost/"
+  # CORRECTED: Removing explicit flags to force HTMLProofer to use the
+  # .htmlproofer.yml file for configuration (which contains disable_external: true).
+  bundle exec htmlproofer "$SITE_DIR"
 }
 
 while (($#)); do
